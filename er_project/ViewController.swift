0d4c04e7ed14cb5cf07e8b806591f9f2e85ec07e
shu2020er <shu2020er@gmail.com>
2020-03-30 09:25:29 +0800

    test1

---
+++ er_project/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  er_project
+//
+//  Created by 陳映亘 on 2020/3/28.
+//  Copyright © 2020 shu2020er. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func startSelect(_ sender: UIButton) {
+    }
+    @IBOutlet var happy: [UIButton]!
+    @IBOutlet var sad: [UIButton]!
+    @IBOutlet var angry: [UIButton]!
+    @IBOutlet var navrous: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
